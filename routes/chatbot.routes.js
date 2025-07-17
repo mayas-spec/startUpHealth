2dd789616c07688fae29b9cd7e82618a5193f9cd
Your Name <your-email@example.com>
2025-07-17 13:31:08 +0000

    chat again

--- routes/chatbot.routes.js
+++ routes/chatbot.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const {handleChatbotQuery}  = require("../controllers/chatbot.controller");
+const { auth } = require("../middlewares/authMiddleware");
 
-router.post('/query', handleChatbotQuery);
+
+router.post('/query',auth,handleChatbotQuery);
 
 module.exports = router;
